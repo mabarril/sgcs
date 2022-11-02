3d4d3e244f842702f10297d0c0f9559037fce68d
mabarril <mabarril@gmail.com>
2022-11-02 16:37:05 -0300

    Modal

---
+++ components/Extrato/index.js
@@ -0,0 +1,66 @@
+import { React, useState, useEffect, Fragment, Row, Button } from "react";
+import { Table } from 'react-bootstrap';
+import axios from "axios";
+import Debito from "../Debito";
+
+export default function Extrato({ idCadastro }) {
+
+    const [debitos, setDebitos] = useState([]);
+    const [incluirDebito, setIncluirDebito] = useState(true);
+
+
+    const getDebito = async (idCadastro) => {
+        await axios.get('https://www.iasdcentraldebrasilia.com.br/cruzeirodosul/sgcs/dbv-api/debitos/' + idCadastro)
+            .then((response) => setDebitos(response.data))
+            .catch((error) => console.error(error));
+        console.log('debitos ', debitos);
+        setIncluirDebito(false);
+    };
+
+    const formataData = (param) => {
+        var data = new Date(param)
+        return data.toLocaleDateString('pt-BR', { timeZone: 'UTC' })
+    }
+
+    const formataMoeda = (valor) => {
+        valor = parseFloat(valor);
+        return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    }
+
+    useEffect(() => {
+        console.log(idCadastro);
+        getDebito(idCadastro);
+    }, [idCadastro]);
+
+    return (
+        debitos.length > 0 ? (<Fragment>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                        <th>Vencimento</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {console.log(debitos.iddebito)}
+                    {debitos.map(debito => {
+                        return (
+                            <tr key={debito.iddebito}>
+                                <td>
+                                    {debito.descdebito}
+                                </td>
+                                <td>{formataMoeda(debito.valordebito)}</td>
+                                <td>{formataData(debito.vctodebito)}</td>
+                                <td>{debito.idpgto ? 'Pago' : <input type={"checkbox"} />}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        </Fragment>
+        ) : <span>Não foram encontrados debitos para o desbravador</span>
+
+    );
+}
